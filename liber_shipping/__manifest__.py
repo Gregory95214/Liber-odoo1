59e364d5a040ba4e910fed1d235a3f393229496f
Gregory95214 <99232851+Gregory95214@users.noreply.github.com>
2025-11-17 19:38:40 -0600

    Create __manifest__.py

---
+++ liber_shipping/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Liber Shipping',
+    'version': '1.0',
+    'summary': 'Integración de Liber Paquetería con Odoo',
+    'author': 'Gregory Rodriguez',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
