fe0ab0c9fc0b0254c5f4a0f5c0718df40cae5993
Gregory95214 <99232851+Gregory95214@users.noreply.github.com>
2025-11-17 21:45:36 -0600

    Update __manifest__.py

--- liber_shipping/__manifest__.py
+++ liber_shipping/__manifest__.py
@@ -9,12 +9,14 @@ Módulo para cotizar y generar guías de Liber Paquetería desde Odoo.
     'category': 'Tools',
     'depends': ['base'],
     'data': [
-    'security/ir.model.access.csv',
-    'views/liber_menus.xml',
-    'views/liber_config_views.xml',
-    'views/liber_quote_views.xml',
-],
+        'security/ir.model.access.csv',
+        'views/liber_menus.xml',
+        'views/liber_config_views.xml',
+        'views/liber_quote_views.xml',
+    ],
     'installable': True,
     'application': True,
+    'license': 'LGPL-3',
 }
+
 
