18a27e48b9c9c905dc98ba66ad15154980ae6baa
Gregory95214 <99232851+Gregory95214@users.noreply.github.com>
2025-11-17 21:46:43 -0600

    Update liber_config.py

--- liber_shipping/models/liber_config.py
+++ liber_shipping/models/liber_config.py
@@ -1,8 +1,11 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields
+
 
 class LiberConfig(models.Model):
     _name = 'liber.config'
-    _description = 'Configuración de Liber Paquetería'
+    _description = 'Configuración Liber Shipping'
 
-    name = fields.Char(string='Nombre configuración', default='Configuración Liber')
+    name = fields.Char(string='Nombre', default='Configuración Liber Shipping')
+    api_url = fields.Char(string='URL de la API')
+    api_key = fields.Char(string='API Key', help='Clave de acceso a la API de Liber')
+
